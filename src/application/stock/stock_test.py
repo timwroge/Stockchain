cb542880a3e0d5de42568f1f6a25f64d2a538b9f
Timothy Wroge <tiw51@pitt.edu>
2020-03-29 23:43:25 -0400

    Revert "Revert "Merge"" Reverts the revert, or does it not do anything at all? 🤔
    This reverts commit 285729ff8d7704403ea429ef3d14367256c33336.

--- src/application/stock/stock_test.py
+++ src/application/stock/stock_test.py
@@ -2,4 +2,4 @@ from Stock import Stock
 google = Stock('TSLA')
 print("The current asking price for "+google.getTicker()+ " is "+str(google.getCurrentPrice()))
 for i in range(100):
-    print("The current asking price for "+google.getTicker()+ " is "+str(google.getCurrentPrice()))
+    print("The current asking price for "+google.getTicker()+ " is "+str(google.getAllInfo()))
