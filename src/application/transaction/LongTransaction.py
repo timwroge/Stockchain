b4c981ffa0b81fd2390b9d2874b72c3a37e513ff
Timothy Wroge <tiw51@pitt.edu>
2020-03-30 01:56:08 -0400

    Add transactions

---
+++ src/application/transaction/LongTransaction.py
@@ -0,0 +1,30 @@
+import datetime as dt
+
+class ShortTransaction(object):
+    def __init__(self, time, shares, stock):
+        '''This is the user object'''
+        self.time = time
+        self.shares = shares
+        self.stock = stock
+
+    def getTime(self):
+        return self.time
+
+    def setTime(self, newTime):
+        self.time  = newTime
+
+    def getShares(self):
+        return self.shares
+
+    def setShares(self, newShares):
+        self.shares = newShares
+
+    def getStock(self):
+        return self.stock
+
+    def setStock(self, newStock):
+        self.stock = newStock
+
+    def perform(self):
+        self.price = self.shares * (  self.getCurrentPrice() -  self.stock.getPrice())
+        return True
