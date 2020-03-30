dac93a9162afec9caf2960fae38697c395df0c07
Timothy Wroge <tiw51@pitt.edu>
2020-03-30 00:17:35 -0400

    Add user class

---
+++ src/application/user/User.py
@@ -0,0 +1,24 @@
+class User(object):
+    def __init__(self, UserId, Username, FullName):
+        '''This is the user object'''
+        self.UserId = UserId
+        self.Username = Username
+        self.FullName = FullName
+
+    def getUserId(self):
+        return self.UserId
+
+    def setUserId(self, newUserId):
+        self.UserId = newUserId
+
+    def getUsername(self):
+        return self.Username
+
+    def setUsername(self, newUsername):
+        self.Username = newUsername
+
+    def getFullName(self):
+        return self.FullName
+
+    def setFullName(self, newFullName):
+        self.FullName  = newFullName
